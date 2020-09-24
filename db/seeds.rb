19d9d9a0090b96556e771e084391c1a9ccdd33d0
pairofAces <karanchauhan16@gmail.com>
2020-09-24 10:35:43 -0400

    added reference code for pagination/data scrapping

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+count = 0 
+while Venue.all.length < 900 do
+    venue_info["_embedded"]["venues"].each do |venue_array|
+        Venue.create(name: venue_array["name"],
+        city: venue_array["city"]["name"],
+        address: venue_array["address"]["line1"])
+    end
+    venue_info = JSON.parse(RestClient.get("https://app.ticketmaster.com/discovery/v2/venues?&locale=*&stateCode=NY&page=#{count}" + "#{karan_api_key}"))
+    count += 1
+end
