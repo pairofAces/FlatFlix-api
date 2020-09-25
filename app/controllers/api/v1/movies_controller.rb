52a6debbf80cae1b4ba373c9ae2d364749b61ac6
Jake Mills <jake.mills.05@gmail.com>
2020-09-25 16:09:26 -0400

    Added preliminary NON WORKING code for sifting through genres dynamically

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -5,6 +5,22 @@ class Api::V1::MoviesController < ApplicationController
 		render json: @movies
 	end
 
+	def show
+		@movie = Movie.find(params[:id])
+		render json: @movie
+	end
+
+	def genres
+		@movies = []
+		@genres
+		Movie.find_each do |movie|
+				if movie.genres.include?("Action")
+					@movies.push(movie)
+				end
+			end
+		render json: @movies
+	end
+
 	private
 
 	def movie_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :watcheds
-  resources :favorites
-  resources :movies
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :watcheds
+      resources :favorites
+      resources :movies
+      resources :users
+      get 'movies/genre/action', to: 'movies#action'
+    end
+  end
 end
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
