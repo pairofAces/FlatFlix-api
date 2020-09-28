4ea71cf98edf44ff46b212a477f557e3419f3e8e
Jake Mills <jake.mills.05@gmail.com>
2020-09-28 19:17:05 -0400

    Started user authentication and switched password to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_012416) do
     t.string "name"
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
