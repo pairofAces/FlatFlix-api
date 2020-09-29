a9be20f3285a509beba7ded097cc1ff74a38d151
Jake Mills <jake.mills.05@gmail.com>
2020-09-29 10:50:10 -0400

    Removed username and moved email to validation param

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,7 @@ class Api::V1::UsersController < ApplicationController
 
 	def create
 		@user = User.create(user_params)
+
 		if @user.valid?
 			@token = encode_token({ user_id: @user.id })
 			render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
@@ -18,7 +19,7 @@ class Api::V1::UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:username, :password, :name, :email)
+		params.require(:user).permit(:name, :email, :password)
 	end
 	
 end
